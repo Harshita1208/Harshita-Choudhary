d70a46ed14505325a725fc258d134eacf54f6b1e
Harshita Choudhary <harshita120896@gmail.com>
2025-04-01 01:06:07 +0530

    Final Changes done

--- assets/toggle-drawer.js
+++ assets/toggle-drawer.js
@@ -1,15 +1,23 @@
-document.getElementById("toggleBtn").addEventListener("click", function (){
+document.addEventListener("DOMContentLoaded", function () {
+  let toggleBtn = document.getElementById("toggleBtn");
   let content = document.querySelector(".toggle-content");
   let menuIcon = document.querySelector(".menu-icon");
   let closeIcon = document.querySelector(".close-icon");
 
-  content.classList.toggle("show");
-
-  if (content.classList.contains("show")){
-    menuIcon.style.display = "none";
-    closeIcon.style.display = "inline-block";
-  } else {
-    menuIcon.style.display = "inline-block";
-    closeIcon.style.display = "none";
+  if (!toggleBtn) {
+    console.error("toggleBtn not found!");
+    return;
   }
+
+  toggleBtn.addEventListener("click", function () {
+    content.classList.toggle("show");
+
+    if (content.classList.contains("show")) {
+      menuIcon.style.display = "none";
+      closeIcon.style.display = "inline-block";
+    } else {
+      menuIcon.style.display = "inline-block";
+      closeIcon.style.display = "none";
+    }
+  });
 });
